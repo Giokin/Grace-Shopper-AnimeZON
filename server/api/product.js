69e7e070a3620b37a46904878dea6ff92ab5d902
Phillip Phifer <38228979+Minsu386@users.noreply.github.com>
2023-06-05 16:14:36 -0400

    Update product.js
    created Product API route

--- server/api/product.js
+++ server/api/product.js
@@ -1,5 +1,56 @@
 const router = require('express').Router()
+const { Product } = require('../db')
 
+// route for all products
+router.get('/', async( req, res, next) => {
+  try{
+    const products = await Product.findAll()
+    res.json(products)
+  } catch(err) {
+    next(err)
+  }
+})
+
+// Get /api/products/:productId
+router.get('/:id', async(req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.json(product)
+  } catch(err) {
+    next(err)
+  }
+})
+
+
+router.post('/', async(req, res, next) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).send(product)
+  }catch(err) {
+    next(err)
+  }
+})
+
+// edit product
+router.put('/:id', async(req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.send(await product.update(req.body));
+  } catch(err) {
+    next(err)
+  }
+})
+
+// delete product
+router.delete('/:productId', async(req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    await product.destry();
+    res.send(product)
+  } catch(err) {
+    next(err)
+  }
+})
 
 
 
